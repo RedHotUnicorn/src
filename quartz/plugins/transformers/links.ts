6179f69306918d0b67aedb198fba880df6ab36d4
RedHotUnicorn <RedHotUnicorn@users.noreply.github.com>
2024-04-14 15:19:34 +0300

    Update links.ts externalLinkIcon

--- quartz/plugins/transformers/links.ts
+++ quartz/plugins/transformers/links.ts
@@ -30,7 +30,7 @@ const defaultOptions: Options = {
   prettyLinks: true,
   openLinksInNewTab: false,
   lazyLoad: false,
-  externalLinkIcon: true,
+  externalLinkIcon: false,
 }
 
 export const CrawlLinks: QuartzTransformerPlugin<Partial<Options> | undefined> = (userOpts) => {
