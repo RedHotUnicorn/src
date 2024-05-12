ae203bb6e7bbebe8c4009251366ca0b6231a90e3
RedHotUnicorn <RedHotUnicorn@users.noreply.github.com>
2024-05-12 13:57:42 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -80,7 +80,7 @@ const config: QuartzConfig = {
       Plugin.ContentIndex({
         enableSiteMap: true,
         enableRSS: true,
-        rssLimit: 100 ,
+        // rssLimit: 100 ,
         rssFullHtml: true ,
       }),
       Plugin.Assets(),
